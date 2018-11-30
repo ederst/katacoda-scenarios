439bd1778b36ea4094f7487a939687f98a10127a
seder <stefan.prietl@bearingpoint.com>
2018-11-30 13:30:02 +0100

    Try the other source

--- hello-world/step1-data.sh
+++ hello-world/step1-data.sh
@@ -1,7 +1,7 @@
 #echo 'shopt -s histappend' >> ~/.bashrc
-#echo 'export PROMPT_COMMAND="history -a;$PROMPT_COMMAND"' >> ~/.history_hack
-#source ~/.history_hack
+echo 'export PROMPT_COMMAND="history -a;$PROMPT_COMMAND"' >> ~/.history_hack
+. ~/.history_hack
 
-echo 'shopt -s histappend' >> ~/.bashrc
-echo 'export PROMPT_COMMAND="history -a;$PROMPT_COMMAND"' >> ~/.bashrc
-bash
+#echo 'shopt -s histappend' >> ~/.bashrc
+#echo 'export PROMPT_COMMAND="history -a;$PROMPT_COMMAND"' >> ~/.bashrc
+#bash
