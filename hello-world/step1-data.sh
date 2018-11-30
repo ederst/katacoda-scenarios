df7e046e9008c05578cd4b05370e54f4beced288
seder <stefan.prietl@bearingpoint.com>
2018-11-30 13:40:54 +0100

    Use code and intro

--- hello-world/step1-data.sh
+++ hello-world/step1-data.sh
@@ -1,6 +1,7 @@
 #echo 'shopt -s histappend' >> ~/.bashrc
-echo 'export PROMPT_COMMAND="history -a;$PROMPT_COMMAND"' >> ~/.history_hack
-. ~/.history_hack
+ echo 'export PROMPT_COMMAND="history -a;$PROMPT_COMMAND"' >> ~/.history_hack
+ . ~/.history_hack
+ clear
 
 #echo 'shopt -s histappend' >> ~/.bashrc
 #echo 'export PROMPT_COMMAND="history -a;$PROMPT_COMMAND"' >> ~/.bashrc
